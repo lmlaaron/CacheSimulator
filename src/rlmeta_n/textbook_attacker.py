5d078e3e023dd3dbfa6dde16c19460e6f3182f7d
mulong <ml2558@cornell.edu>
2024-11-25 06:37:26 +0000

    update

--- src/rlmeta_n/textbook_attacker.py
+++ src/rlmeta_n/textbook_attacker.py
@@ -7,6 +7,8 @@
 # which can have high reward for the cache guessing game
 # used to generate the attack sequence that can be detected by cchunter
 # currently it only works for the direct-map cache (associativity=1)
+
+import random
 class TextbookAgent():
 
     # the config is the same as the config cor cache_guessing_game_env_impl
@@ -87,6 +89,52 @@ class TextbookAgent():
         return
 
 
+
+class RandomAgent():
+
+    # the config is the same as the config cor cache_guessing_game_env_impl
+    def __init__(self, env_config):
+        random.seed()
+        self.local_step = 0
+        self.lat = []
+        self.no_prime = False # set to true after first prime
+        if "cache_configs" in env_config:
+            #self.logger.info('Load config from JSON')
+            self.configs = env_config["cache_configs"]
+            self.num_ways = self.configs['cache_1']['associativity'] 
+            self.cache_size = self.configs['cache_1']['blocks']
+            self.attacker_addr_s = env_config["attacker_addr_s"] if "attacker_addr_s" in env_config else 4
+            self.attacker_addr_e = env_config["attacker_addr_e"] if "attacker_addr_e" in env_config else 7
+            victim_addr_s = env_config["victim_addr_s"] if "victim_addr_s" in env_config else 0
+            victim_addr_e = env_config["victim_addr_e"] if "victim_addr_e" in env_config else 3
+            flush_inst = env_config["flush_inst"] if "flush_inst" in env_config else False            
+            self.allow_empty_victim_access = env_config["allow_empty_victim_access"] if "allow_empty_victim_access" in env_config else False
+            
+    # initialize the agent with an observation
+    def observe_init(self, timestep):
+        # initialization doing nothing
+        self.local_step = 0
+        self.lat = []
+        self.no_prime = False
+        return
+
+
+    # returns an action
+    def act(self, timestep):
+        info = {}
+        # do prime
+        action = random.randint(0, 1 + self.attacker_addr_e - self.attacker_addr_s) 
+        self.local_step += 1
+        return action, info
+
+    # is it useful for non-ML agent or not???
+    def observe(self, action, timestep):
+        ####if self.local_step < 2 * self.cache_size + 1 + 1 - (self.cache_size if self.no_prime else 0 ) and self.local_step > self.cache_size - (self.cache_size if self.no_prime else 0 ):#- 1:
+        ######    self.local_step += 1
+        ####    self.lat.append(timestep.observation[0][0])
+        return
+
+ 
 
 class OccupancyAgent():
 
